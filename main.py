2b34bc695009452684c2c34425dc1206dcd9a40e
Visslyy <maksimslepuch@gmail.com>
2025-05-11 16:26:47 +0300

    main.py update + files

--- main.py
+++ main.py
@@ -1,60 +1,64 @@
 import pygame
 
+class BaseSprite:
+    def __init__(self, x, y, width, height, speed, texture):
+        self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, [width, height])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+class Hero(BaseSprite):
+    def update(self, window):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.hitbox.x -= self.speed
+        if keys[pygame.K_d]:
+            self.hitbox.x += self.speed
+        if keys[pygame.K_w]:
+            self.hitbox.y -= self.speed
+        if keys[pygame.K_s]:
+            self.hitbox.y += self.speed
+        self.draw(window)
+    
+    def check_coordinates(self):
+        print(self.hitbox)
+    
+class Wall:
+    def __init__(self, x, y, width, height, color):
+        self.hitbox = pygame.Rect(x, y, width, height)
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.color = color
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.hitbox)
+
 pygame.init()
 
 window = pygame.display.set_mode([1080, 640])
 fps = pygame.time.Clock()
-background_img = pygame.image.load("images/background.png")
+background_img = pygame.image.load("images/background.jpg")
 background_img = pygame.transform.scale(background_img, [1080, 640])
-text = pygame.font.Font(None, 30).render("Text", True, [0, 0, 0])
-class Player:
-    def __init__(self, texture_path):
-        self.x, self.y = 120, 60
-        self.texture_size = [50, 50]
-        self.texture_path = texture_path
-        self.texture_load = pygame.image.load(f"images/{self.texture_path}")
-        self.texture = pygame.transform.scale(self.texture_load, self.texture_size)
-
-    def draw(self):
-        window.blit(self.texture, [self.x, self.y])
-        if self.x >= 1040:
-            self.x = 1040
-        if self.x <= 0:
-            self.x = 0
-        if self.y >= 600:
-            self.y = 600
-        if self.y <= 0:
-            self.y = 0
-
+hero = Hero(15, 35, 50, 50, 5, "images/hero.png")
+walls = [
+    Wall(0, 0, 210, 30, [35, 117, 2]),
+    Wall(0, 80, 130, 700, [35, 117, 2])
+]
+window.blit(background_img, [0, 0])
 running = True
-player1 = Player("sprite1.png")
-player2 = Player("sprite2.png")
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        player1.x += 5
-    if keys[pygame.K_a]:
-        player1.x -= 5
-    if keys[pygame.K_w]:
-        player1.y -= 5
-    if keys[pygame.K_s]:
-        player1.y += 5
-    if keys[pygame.K_RIGHT]:
-        player2.x += 5
-    if keys[pygame.K_LEFT]:
-        player2.x -= 5
-    if keys[pygame.K_UP]:
-        player2.y -= 5
-    if keys[pygame.K_DOWN]:
-        player2.y += 5
-    window.blit(background_img, [0, 0])
-    window.blit(text, [20, 20])
-    player1.draw()
-    player2.draw()
-    print(player1.y)
+    for wall in walls:
+        wall.draw(window)
+    hero.update(window)
+    hero.check_coordinates()
     pygame.display.flip()
     fps.tick(60)
